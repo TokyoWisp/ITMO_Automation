cb48863a9ecb53467564e3a47d27bba71fa377f0
Tokyo_Wisp <annavacances86@gmail.com>
2024-07-30 16:25:16 +0300

    First Commit

---
+++ python_trening/task_8_if_elif_else.py
@@ -0,0 +1,21 @@
+num_float = 3.4
+
+if num_float > 0:
+    print('Положительное число')
+elif num_float == 0:
+    print('Ноль')
+else:
+    print('Отрицательное число')
+
+
+def study(year):
+    if year <= 4:
+        print('Бакалавр')
+    elif year in range(5, 6):
+        print('Магистр')
+    elif year in range(7, 8, 9):
+        print('Аспирант')
+    else:
+        print('Введите корректный год обучения')
+
+study(4)
